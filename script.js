c06807d43383cf07ab325bfbba3d1a0d843646af
kantamarajuSakethrao <sakethkantamaraju.13@gmail.com>
2026-02-18 23:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ class Rectangle {
 	private _width : number;
 	private _height : number;
 	
-	constructor(width : number, height : number) {
-		this._width = width;
-		this._height = height;
+	constructor(width: number, height: number) {
+	if (width <= 0 || height <= 0) {
+		throw new Error("Width and Height must be positive integers");
 	}
+	this._width = width;
+	this._height = height;
+}
+
 	
 	get width() : number{
 		return this._width;
